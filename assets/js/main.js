697bc62236682ddd9b24a0905ed3ab9565c65895
MiguelG619 <69783877+MiguelG619@users.noreply.github.com>
2022-03-19 13:57:48 +0800

    Implemented scroll up js and styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,7 +130,25 @@ function scrollActive() {
 window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header');
 
+    // Add the scroll-header class to the header tag when the scroll is greater than 80 viewport height
+    if (this.scrollY >= 80) 
+        nav.classList.add('scroll-header');
+    else
+        nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
 
+  // Add the show-scroll class when the scroll is higher than 560 viewport height
+  if (this.scrollY >= 560) 
+    scrollUp.classList.add('show-scroll');
+else 
+    scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 /*==================== DARK LIGHT THEME ====================*/
